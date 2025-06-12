6b1b50b315a69cbbff3b72fa71953bba08230ac3
Руслан Рахматуллин <gfastg98@gmail.com>
2025-06-12 12:54:23 +0300

    Обновление kotlin Обновление dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "ru.gfastg98.qr_scanner_compose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.gfastg98.qr_scanner_compose"
@@ -39,9 +40,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,61 +48,49 @@ android {
 }
 
 dependencies {
-
-
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     implementation ("com.github.androidmads:QRGenerator:1.0.1")
+    val sqliteVersion = "2.5.1"
 
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    val sqlite_version = "2.4.0"
+    // SQLite
+    implementation("androidx.sqlite:sqlite-ktx:$sqliteVersion")
+    // Implementation of the AndroidX SQLite interfaces via the Android framework APIs.
+    implementation("androidx.sqlite:sqlite-framework:$sqliteVersion")
 
-        // Java language implementation
-    //implementation("androidx.sqlite:sqlite:$sqlite_version")
-
-        // Kotlin
-    implementation("androidx.sqlite:sqlite-ktx:$sqlite_version")
-
-        // Implementation of the AndroidX SQLite interfaces via the Android framework APIs.
-    implementation("androidx.sqlite:sqlite-framework:$sqlite_version")
-
-
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
-
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    //compose
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    val cameraXVersion = "1.3.0-rc01"
+    implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.1")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    val cameraXVersion = "1.4.2"
     implementation("androidx.camera:camera-core:$cameraXVersion")
     implementation("androidx.camera:camera-camera2:$cameraXVersion")
     implementation("androidx.camera:camera-view:$cameraXVersion")
-
     implementation("androidx.camera:camera-extensions:$cameraXVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraXVersion")
-    //implementation ("com.google.android.gms:play-services-vision:20.1.3")
-    implementation ("com.google.mlkit:barcode-scanning:17.2.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 
-    val activity_version = "1.8.2"
+    val activity_version = "1.10.1"
         // Kotlin
     implementation("androidx.activity:activity-ktx:$activity_version")
 
