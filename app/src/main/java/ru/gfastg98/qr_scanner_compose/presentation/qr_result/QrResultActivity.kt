0c95e3d2a609c261dbd72adc4d98a36961144f0f
Руслан Рахматуллин <gfastg98@gmail.com>
2026-01-04 17:06:13 +0300

    Добавление IntentBuilder в QrResultActivity
    Добавлены константы для extras (EXTRA_FILE_NAME, EXTRA_CONTENT, EXTRA_GENERATED, EXTRA_BARCODE_OBJECT, EXTRA_VIEW).
    Реализован класс IntentBuilder для удобного создания и запуска интента QrResultActivity с необходимыми параметрами.

--- app/src/main/java/ru/gfastg98/qr_scanner_compose/presentation/qr_result/QrResultActivity.kt
+++ app/src/main/java/ru/gfastg98/qr_scanner_compose/presentation/qr_result/QrResultActivity.kt
@@ -1,5 +1,7 @@
 package ru.gfastg98.qr_scanner_compose.presentation.qr_result
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +11,12 @@ private val TAG = QrResultActivity::class.java.simpleName
 
 class QrResultActivity : ComponentActivity() {
     companion object {
+        const val EXTRA_FILE_NAME = "file_name"
+        const val EXTRA_CONTENT = "content"
         const val EXTRA_CODE_FORMAT = "code_format"
+        const val EXTRA_GENERATED = "generated"
+        const val EXTRA_BARCODE_OBJECT = "barcode_obj"
+        const val EXTRA_VIEW = "view"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +27,45 @@ class QrResultActivity : ComponentActivity() {
             }
         }
     }
-}
 
+    class IntentBuilder(private val context: Context) {
+        private val intent = Intent(
+            context,
+            QrResultActivity::class.java
+        ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        fun setFileName(file: String): IntentBuilder {
+            intent.putExtra(EXTRA_FILE_NAME, file)
+            return this
+        }
+
+        fun setContent(content: String): IntentBuilder {
+            intent.putExtra(EXTRA_CONTENT, content)
+            return this
+        }
+
+        fun setGenerated(generated: Boolean): IntentBuilder {
+            intent.putExtra(EXTRA_GENERATED, generated)
+            return this
+        }
+
+        fun setBarcodeObjectJson(barcodeObj: String): IntentBuilder {
+            intent.putExtra(EXTRA_BARCODE_OBJECT, barcodeObj)
+            return this
+        }
+
+        fun setCodeFormat(codeFormat: Int): IntentBuilder {
+            intent.putExtra(EXTRA_CODE_FORMAT, codeFormat)
+            return this
+        }
+
+        fun setIsView(isView: Boolean): IntentBuilder {
+            intent.putExtra(EXTRA_VIEW, isView)
+            return this
+        }
+
+        fun build(): Intent = intent
+
+        fun launch() = context.startActivity(intent)
+    }
+}
